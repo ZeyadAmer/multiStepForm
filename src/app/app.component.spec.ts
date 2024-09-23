7e195593ecf95e2ca74984d7db3348663c0c1184
zeyad amer <zeyadamer285@gmail.com>
2024-09-23 12:38:05 +0300

    form pages done

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,7 +4,9 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'multiStepForm' title`, () => {
+  it(`should have as title 'angular-complex-reactive-forms'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('multiStepForm');
+    expect(app.title).toEqual('angular-complex-reactive-forms');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, multiStepForm');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-complex-reactive-forms app is running!');
   });
 });
---
+++ src/app/form/product-form/ref-data.ts
@@ -0,0 +1,28 @@
+export const categories = [
+  'Living room',
+  'Bathroom',
+  'Kitchen',
+  'Skin care',
+  'Hair care',
+  'Pets',
+  'Jewelry',
+  'Clothes',
+];
+
+export const currencies = ['EGP','EUR', 'USD', 'GBP'];
+
+export const distributionChannels = [
+  'Customer',
+  'B2B',
+  'Drop Shipping',
+  'Wholesale',
+  'E-commerce',
+  'Direct Sale',
+];
+
+export const dummyAcknowledgements = [
+  { key: '001', value: 'I hereby acknowledge, that I have read and understood the conditions' },
+  { key: '002', value: 'I am responsible to track my package' },
+  { key: '003', value: 'I want to get mails for future offers' },
+  { key: '004', value: 'I agree to show my id when recieving the order' },
+];
